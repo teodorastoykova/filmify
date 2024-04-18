fcb366edaaed62ca9ce64d188b23d168bcf5a4af
teodora stoykowa <t.stoykowa@gmail.com>
2024-04-18 18:33:12 +0300

    new service

---
+++ src/Services/Movies/WatchListMoviesService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const getWatchListMovies = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/account/21198834/watchlist/movies",
+      {
+        params: {
+          language: "en-US",
+          page: "1",
+          session_id: localStorage.getItem('sessionId'),
+          sort_by: "created_at.asc",
+        },
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NThkYzk1YTAxNDQyY2NmZDM0YzJlOWY0MmRiNjY5MCIsInN1YiI6IjY2MTNlOGU5MGJiMDc2MDE0ODJmYjYzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W6UGD_8V7MgsHRv2EizIyg5KV9ZZb9cHGV4A_Nq_UNY",
+        },
+      }
+    );
+    return response.data.results;
+  } catch (error) {
+    throw new Error("Failed to fetch watch list movies: ", error);
+  }
+
+};
+
+export default getWatchListMovies;
