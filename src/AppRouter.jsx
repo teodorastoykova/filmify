34c789c8b8c0412f4d0a9a799ac6ba22ffb0b192
teodora stoykowa <t.stoykowa@gmail.com>
2024-04-19 18:57:00 +0300

    -

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,25 +1,22 @@
 /* eslint-disable react/prop-types */
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from "./components/common/Home";
-import About from "./components/public/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import MovieDetails from "./components/movies/MovieDetails";
 import TvSeriesDetails from "./components/tvSeries/TvSeriesDetails";
-import Login from "./components/public/Login";
-import SignUp from "./components/public/SignUp";
-import Browse from "./components/common/Browse"
-import WatchList from "./components/common/WatchList";
-import TopRated from "./components/common/TopRatedList";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Browse from "./pages/Browse";
+import WatchList from "./pages/WatchList";
+import TopRated from "./pages/TopRatedList";
+
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/top_rated" element={<TopRated />} />
-        <Route path="/my_watch_list" element={<WatchList/>} />
+        <Route path="/my_watch_list" element={<WatchList />} />
         <Route path="/movies/:movieId" element={<MovieDetails />} />
         <Route path="/series/:seriesId" element={<TvSeriesDetails />} />
         <Route path="/browse" element={<Browse />} />
