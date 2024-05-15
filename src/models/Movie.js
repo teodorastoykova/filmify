249bc94eca64e5371ed01ca9157010acdfe22cfd
teodora stoykowa <t.stoykowa@gmail.com>
2024-05-15 11:12:29 +0300

    mediaType att implemented

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,13 +1,21 @@
-
 export default class Movie {
-    constructor(id, title, overview, posterPath, releaseDate, voteAverage, productionCompanies) {
-      this.id = id;
-      if (title) this.title = title;
-      if (posterPath) this.posterPath = posterPath;
-      if (overview) this.overview = overview;
-      if (releaseDate) this.releaseDate = releaseDate;
-      if (voteAverage) this.voteAverage = voteAverage;
-      if (productionCompanies) this.productionCompanies = productionCompanies;
-    }
+  constructor(
+    id,
+    title,
+    overview,
+    posterPath,
+    releaseDate,
+    voteAverage,
+    productionCompanies,
+    mediaType
+  ) {
+    this.id = id;
+    if (title) this.title = title;
+    if (posterPath) this.posterPath = posterPath;
+    if (overview) this.overview = overview;
+    if (releaseDate) this.releaseDate = releaseDate;
+    if (voteAverage) this.voteAverage = voteAverage;
+    if (productionCompanies) this.productionCompanies = productionCompanies;
+    if (mediaType) this.mediaType = mediaType;
   }
-  
+}
--- src/models/TvSeries.js
+++ src/models/TvSeries.js
@@ -1,13 +1,25 @@
 export default class TvSeries {
-    constructor(id, title, overview, posterPath, releaseDate, voteAverage, seasons, episodes, productionCompanies) {
-      this.id = id;
-      if (title) this.title = title;
-      if (overview) this.overview = overview;
-      if (posterPath) this.posterPath = posterPath;
-      if (releaseDate) this.releaseDate = releaseDate;
-      if (voteAverage) this.voteAverage = voteAverage;
-      if (seasons) this.seasons = seasons;
-      if (episodes) this.episodes = episodes;
-      if (productionCompanies) this.productionCompanies = productionCompanies;
-    }
+  constructor(
+    id,
+    title,
+    overview,
+    posterPath,
+    releaseDate,
+    voteAverage,
+    seasons,
+    episodes,
+    productionCompanies,
+    mediaType
+  ) {
+    this.id = id;
+    if (title) this.title = title;
+    if (overview) this.overview = overview;
+    if (posterPath) this.posterPath = posterPath;
+    if (releaseDate) this.releaseDate = releaseDate;
+    if (voteAverage) this.voteAverage = voteAverage;
+    if (seasons) this.seasons = seasons;
+    if (episodes) this.episodes = episodes;
+    if (productionCompanies) this.productionCompanies = productionCompanies;
+    if (mediaType) this.mediaType = mediaType;
   }
+}
